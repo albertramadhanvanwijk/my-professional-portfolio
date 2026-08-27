2a105beef09c61d2755c3f5ddabeb54b6828e795
Albert Ramadhan Van Wijk <albertramadhanvanwijk@gmail.com>
2026-08-27 15:59:06 +0700

    fix: routes API

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -1,56 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
-
-    Route::get('/', function () {
-        return view('welcome');
-    });
-
-    Route::get('/profile', [
-        PortfolioController::class,
-        'profile',
-    ]);
-
-    Route::get('/experiences', [
-        PortfolioController::class,
-        'experiences',
-    ]);
-
-    Route::get('/education', [
-        PortfolioController::class,
-        'education',
-    ]);
-
-    Route::get('/skills', [
-        PortfolioController::class,
-        'skills',
-    ]);
-
-    Route::get('/projects', [
-        PortfolioController::class,
-        'projects',
-    ]);
-
-    Route::get('/projects/{project:slug}', [
-        PortfolioController::class,
-        'project',
-    ]);
-
-    Route::get('/certifications', [
-        PortfolioController::class,
-        'certifications',
-    ]);
-
-    Route::get('/achievements', [
-        PortfolioController::class,
-        'achievements',
-    ]);
-
-    Route::get('/social-links', [
-        PortfolioController::class,
-        'socialLinks',
-    ]);
+Route::get('/', function () {
+    return view('welcome');
 });
